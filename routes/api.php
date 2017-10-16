1c6bd6156991f54857c7432dc34d902880951b7a
Adonias Delmiro Dantas Neto <adonias.dantas@academico.ifrn.edu.br>
2017-10-15 22:36:16 -0300

    adicionado o documento inicial da documentação da api

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,7 @@ Route::group(['prefix' => '/v1'], function() {
 
     Route::resource('obras', 'ObraController');
     Route::resource('obras.capitulos', 'CapituloController');
+    Route::post('register', 'Auth\RegisterController@register')->middleware('guest');
+    Route::post('login', 'Auth\LoginController@login')->middleware('guest');
+    Route::post('logout', 'Auth\LoginController@logout')->middleware('auth');
 });
-
-Route::post('register', 'Auth\RegisterController@register')->middleware('guest');
-Route::post('login', 'Auth\LoginController@login')->middleware('guest');
-Route::post('logout', 'Auth\LoginController@logout')->middleware('auth');
