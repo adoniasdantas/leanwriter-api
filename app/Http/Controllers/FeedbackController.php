12cd76e56358eaea62b5edc404dfb969ed5c878f
Adonias Delmiro Dantas Neto <adoniasdantas@gmail.com>
2017-11-28 10:42:11 -0300

    implementado destroy dos feedbacks

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -106,8 +106,10 @@ class FeedbackController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, Obra $obra, Capitulo $capitulo, Feedback $feedback)
     {
-        //
+        $feedback->delete();
+
+        return 204;
     }
 }
