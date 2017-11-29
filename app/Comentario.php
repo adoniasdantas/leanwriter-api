b244ad190258df1b5f91ca66b52bc6f59698e604
Adonias Delmiro Dantas Neto <adoniasdantas@gmail.com>
2017-11-29 08:13:00 -0300

    implementados os metodos do model Comentario

--- app/Comentario.php
+++ app/Comentario.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use App\User;
+use App\Obra;
 use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    protected $table = 'comentarios';
+
+    protected $fillable = ['texto', 'obra_id', 'user_id'];
+
+    public function obra()
+    {
+        return $this->belongsTo(Obra::class);
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
